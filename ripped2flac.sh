6094b3c9fb4a5feedb4be4009c54eb634d3529e6
Michel Hoogervorst <michel@highking.nl>
2023-07-17 22:21:33 +0200

    Neeeewer versjuhn

--- ripped2flac.sh
+++ ripped2flac.sh
@@ -6,24 +6,24 @@
 # jacktheripper.sh and converts them to FLAC.
 #
 # Author: Michel Hoogervorst <michel@highking.nl>
-# Date: 9 december 2019 
+#
 
 [ -f /home/public/jacktheripper.log ] || exit 1
 [ -f /home/public/jacktheripper.log.1 ] && exit 1
 mv /home/public/jacktheripper.log /home/public/jacktheripper.log.1
 for id in $(</home/public/jacktheripper.log.1); do  
-
   # Try to open the directory with this id
   cd /home/public/rip$id || exit 1
 
   # Get album information
   albumartist=$(grep Albumperformer audio_01.inf | cut -d"'" -f2- | sed "s/'$//g")
   album=$(grep Albumtitle audio_01.inf | cut -d"'" -f2- | sed "s/'$//g")
+  albumdir=$(echo "$album" | sed "s/\//_/g")
   year=$(grep DYEAR audio.cddb | cut -d'=' -f2)
-  genre=$(grep DGENRE audio.cddb | cut -d'=' -f2)
+  genres=$(grep DGENRE audio.cddb | cut -d'=' -f2)
 
   # Create the directory for this album
-  mkdir -p "$albumartist/$album" || exit 1
+  mkdir -p "$albumartist/$albumdir" || exit 1
 
   # Convert all .wav files to .flac
   for track in *wav; do
@@ -39,34 +39,48 @@ for id in $(</home/public/jacktheripper.log.1); do
     fi
 
     # Convert the .wav to .flac
-    flac -8 $track -o "$albumartist/$album/$tracknumber - $title.flac" || exit 1
+    flac -8 $track -o "$albumartist/$albumdir/$tracknumber - $title.flac" || exit 1
 
     # Add metadata to the file
-    metaflac --set-tag=ARTIST="$artist" "$albumartist/$album/$tracknumber - $title.flac"
-    metaflac --set-tag=ALBUM="$album" "$albumartist/$album/$tracknumber - $title.flac"
-    metaflac --set-tag=TITLE="$title" "$albumartist/$album/$tracknumber - $title.flac"
-    metaflac --set-tag=TRACKNUMBER="$tracknumber" "$albumartist/$album/$tracknumber - $title.flac"
-    [ -z ${genre+x} ] || metaflac --set-tag=GENRE="$genre" "$albumartist/$album/$tracknumber - $title.flac"
-    [ -z ${year+x}  ] || metaflac --set-tag=DATE="$year" "$albumartist/$album/$tracknumber - $title.flac"
+    metaflac --set-tag=ARTIST="$artist" "$albumartist/$albumdir/$tracknumber - $title.flac"
+    metaflac --set-tag=ALBUM="$album" "$albumartist/$albumdir/$tracknumber - $title.flac"
+    metaflac --set-tag=TITLE="$title" "$albumartist/$albumdir/$tracknumber - $title.flac"
+    metaflac --set-tag=TRACKNUMBER="$tracknumber" "$albumartist/$albumdir/$tracknumber - $title.flac"
+    [ -z ${year+x}  ] || metaflac --set-tag=DATE="$year" "$albumartist/$albumdir/$tracknumber - $title.flac"
+
+    # Add genre tag(s), can be space delimited
+    if [ ! -z ${genres+x} ]; then
+      IFS=";"
+      for genre in ${genres// / }; do
+          metaflac --set-tag=GENRE="$genre" "$albumartist/$albumdir/$tracknumber - $title.flac"
+      done
+    fi
 
   done
 
   # Add ReplayGain tags to the files (this has to be done for the whole album at once)
-  pushd "$albumartist/$album"
+  pushd "$albumartist/$albumdir"
   metaflac --add-replay-gain *flac
   popd
 
+  # Copy cover file, if it exists
+  for coverart in cover.jpg cover.png; do
+    if [ -f $coverart ]; then
+      cp $coverart "$albumartist/$albumdir/"
+    fi
+  done
+
   # Move converted files to our music collection
   if [ -d "/home/public/Muziek/$albumartist" ]; then
-    mv "$albumartist/$album" "/home/public/Muziek/$albumartist/$album"
+    mv "$albumartist/$albumdir" "/home/public/Muziek/$albumartist/$albumdir"
   else
     mv "$albumartist" "/home/public/Muziek/$albumartist"
   fi
 
   # Remove original rip
-  if [ "$?" == "0" ]; then
-    cd ~
-    rm -rf /home/public/rip$id
-  fi
+  #if [ "$?" == "0" ]; then
+  #  cd ~
+  #  rm -rf /home/public/rip$id
+  #fi
 
 done && rm -f /home/public/jacktheripper.log.1
